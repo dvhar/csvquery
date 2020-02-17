c65a24fdae083ebbc22c6b161974b71ee9e3cc11
dvhar <dh22@protonmail.com>
2020-02-16 20:42:41 -0600

    vmachine uses computed goto

--- vmachine.cc
+++ vmachine.cc
@@ -12,7 +12,16 @@
 #define push() ++stacktop
 #define pop() --stacktop
 
+
+
+#define next() \
+	op = ops + ip; \
+	goto *(labels[op->code]); \
+	//cerr << ft("ip {} opcode {} stack {}\n", ip, opMap[op->code], stacktop-stack.data());
+
 void vmachine::run(){
+
+	void* labels[] = { &&CVER_label, &&CVNO_label, &&CVIF_label, &&CVIS_label, &&CVFI_label, &&CVFS_label, &&CVDRS_label, &&CVDRF_label, &&CVDTS_label, &&CVSI_label, &&CVSF_label, &&CVSDR_label, &&CVSDT_label, &&IADD_label, &&FADD_label, &&TADD_label, &&DTADD_label, &&DRADD_label, &&ISUB_label, &&FSUB_label, &&DTSUB_label, &&DRSUB_label, &&IMULT_label, &&FMULT_label, &&DRMULT_label, &&IDIV_label, &&FDIV_label, &&DRDIV_label, &&INEG_label, &&FNEG_label, &&DNEG_label, &&PNEG_label, &&IMOD_label, &&IEXP_label, &&FEXP_label, &&JMP_label, &&JMPCNT_label, &&JMPTRUE_label, &&JMPFALSE_label, &&JMPNOTNULL_ELSEPOP_label, &&RDLINE_label, &&RDLINE_ORDERED_label, &&PREP_REREAD_label, &&PUT_label, &&LDPUT_label, &&LDPUTALL_label, &&PUTVAR_label, &&LDINT_label, &&LDFLOAT_label, &&LDTEXT_label, &&LDDATE_label, &&LDDUR_label, &&LDNULL_label, &&LDLIT_label, &&LDVAR_label, &&IEQ_label, &&FEQ_label, &&TEQ_label, &&NEQ_label, &&LIKE_label, &&ILEQ_label, &&FLEQ_label, &&TLEQ_label, &&ILT_label, &&FLT_label, &&TLT_label, &&PRINT_label, &&POP_label, &&POPCPY_label, &&ENDRUN_label, &&NULFALSE1_label, &&NULFALSE2_label, &&NDIST_label, &&SDIST_label, &&PUTDIST_label, &&FINC_label, &&ENCCHA_label, &&DECCHA_label, &&SAVEPOSI_JMP_label, &&SAVEPOSF_JMP_label, &&SAVEPOSS_JMP_label, &&SORTI_label, &&SORTF_label, &&SORTS_label };
 
 	int64 i64Temp;
 	int iTemp1, iTemp2;
@@ -28,29 +37,25 @@ void vmachine::run(){
 	int ip = 0;
 	opcode *op;
 
-	for (;;){
-		op = ops + ip;
-		//cerr << ft("ip {} opcode {} stack {}\n", ip, opMap[op->code], stacktop-stack.data());
-		//big switch is flush-left instead of using normal indentation
-		switch(op->code){
+	next();
 
 //put data from stack into torow
 //should make a version of this that prints directly rather than using torow
-case PUT:
+PUT_label:
 	FREE1(torow[op->p1]);
 	torow[op->p1] = stk0;
 	DISOWN(stk0);
 	pop();
 	++ip;
-	break;
+	next();
 //put data from filereader directly into torow
-case LDPUT:
+LDPUT_label:
 	csvTemp = files[op->p3]->entries[op->p2];
 	FREE1(torow[op->p1]);
 	torow[op->p1] = dat{ { .s = csvTemp.val }, T, csvTemp.size };
 	++ip;
-	break;
-case LDPUTALL:
+	next();
+LDPUTALL_label:
 	iTemp1 = op->p1;
 	for (auto &f : files){
 		for (auto &e : f->entries){
@@ -59,84 +64,84 @@ case LDPUTALL:
 		}
 	}
 	++ip;
-	break;
-case PUTDIST:
+	next();
+PUTDIST_label:
 	FREE1(torow[op->p1]);
 	torow[op->p1] = distinctVal;
 	DISOWN(distinctVal);
 	++ip;
-	break;
+	next();
 //put variable from stack into var vector
-case PUTVAR:
+PUTVAR_label:
 	FREE1(vars[op->p1]);
 	vars[op->p1] = stk0;
 	DISOWN(stk0);
 	pop();
 	++ip;
-	break;
+	next();
 
 //put variable from var vector into stack
-case LDVAR:
+LDVAR_label:
 	push();
 	stk0 = vars[op->p1];
 	DISOWN(stk0); //var vector still owns c string
 	++ip;
-	break;
+	next();
 //load data from filereader to the stack
-case LDDUR:
+LDDUR_label:
 	push();
 	iTemp1 = parseDuration(files[op->p1]->entries[op->p2].val, &i64Temp);
 	stk0 = dat{ { .i = i64Temp}, DR};
 	if (iTemp1) stk0.b |= NIL;
 	++ip;
-	break;
-case LDDATE:
+	next();
+LDDATE_label:
 	push();
 	csvTemp = files[op->p1]->entries[op->p2];
 	iTemp1 = dateparse(csvTemp.val, &i64Temp, &iTemp2, csvTemp.size);
 	stk0 = dat{ { .i = i64Temp}, DT, iTemp2 };
 	if (iTemp1) stk0.b |= NIL;
 	++ip;
-	break;
-case LDTEXT:
+	next();
+LDTEXT_label:
 	push();
 	csvTemp = files[op->p1]->entries[op->p2];
 	stk0 = dat{ { .s = csvTemp.val }, T, csvTemp.size };
 	if (!csvTemp.size) stk0.b |= NIL;
 	++ip;
-	break;
-case LDFLOAT:
+	next();
+LDFLOAT_label:
 	push();
 	csvTemp = files[op->p1]->entries[op->p2];
 	stk0.u.f = strtof(csvTemp.val, &cstrTemp);
 	stk0.b = F;
 	if (!csvTemp.size || *cstrTemp){ stk0.b |= NIL; }
 	++ip;
-	break;
-case LDINT:
+	next();
+LDINT_label:
 	push();
 	csvTemp = files[op->p1]->entries[op->p2];
 	stk0.u.i = strtol(csvTemp.val, &cstrTemp, 10);
 	stk0.b = I;
 	if (!csvTemp.size || *cstrTemp) stk0.b |= NIL;
 	++ip;
-	break;
-case LDNULL:
+	next();
+LDNULL_label:
 	push();
 	stk0.b = NIL;
 	++ip;
-	break;
-case LDLIT:
+	next();
+LDLIT_label:
 	push();
 	stk0 = q->dataholder[op->p1];
 	++ip;
-	break;
+	next();
 
 //read a new line from a file
-case RDLINE:
+RDLINE_label:
 	ip = files[op->p2]->readline() ? op->p1 : ip+1;
-	break;
-case RDLINE_ORDERED:
+	next();
+RDLINE_ORDERED_label:
 	//stk0 has current read index, stk1 has vector.size()
 	if (stk0.u.i < stk1.u.i){
 		files[op->p2]->readlineat(posVectors[op->p3][stk0.u.i++].pos);
@@ -144,23 +149,23 @@ case RDLINE_ORDERED:
 	} else {
 		ip = op->p1;
 	}
-	break;
-case PREP_REREAD:
+	next();
+PREP_REREAD_label:
 	push();
 	stk0.u.i = posVectors[op->p1].size();
 	push();
 	stk0.u.i = 0;
 	++ip;
-	break;
-case SAVEPOSI_JMP:
+	next();
+SAVEPOSI_JMP_label:
 	posVectors[op->p2].push_back(valPos( stk0.u.i, files[op->p3]->pos ));
 	ip = op->p1;
-	break;
-case SAVEPOSF_JMP:
+	next();
+SAVEPOSF_JMP_label:
 	posVectors[op->p2].push_back(valPos( stk0.u.f, files[op->p3]->pos ));
 	ip = op->p1;
-	break;
-case SAVEPOSS_JMP:
+	next();
+SAVEPOSS_JMP_label:
 	if (ISMAL(stk0)){
 		cstrTemp = stk0.u.s;
 		DISOWN(stk0);
@@ -170,110 +175,110 @@ case SAVEPOSS_JMP:
 	}
 	posVectors[op->p2].push_back(valPos( cstrTemp, files[op->p3]->pos ));
 	ip = op->p1;
-	break;
+	next();
 
 #ifndef __APPLE__ //get your shit together, apple
-case SORTI:
+SORTI_label:
 	sort(execution::par_unseq, posVectors[op->p1].begin(), posVectors[op->p1].end(),
 		[op](const valPos &a, const valPos &b){ return (a.val.i > b.val.i)^op->p2; });
 	++ip;
-	break;
-case SORTF:
+	next();
+SORTF_label:
 	sort(execution::par_unseq, posVectors[op->p1].begin(), posVectors[op->p1].end(),
 		[op](const valPos &a, const valPos &b){ return (a.val.f > b.val.f)^op->p2; });
 	++ip;
-	break;
-case SORTS:
+	next();
+SORTS_label:
 	sort(execution::par_unseq, posVectors[op->p1].begin(), posVectors[op->p1].end(),
 		[op](const valPos &a, const valPos &b){ return (scomp(a.val.s, b.val.s) > 0)^op->p2; });
 	++ip;
-	break;
+	next();
 #else
-case SORTI:
+SORTI_label:
 	sort(posVectors[op->p1].begin(), posVectors[op->p1].end(),
 		[op](const valPos &a, const valPos &b){ return (a.val.i > b.val.i)^op->p2; });
 	++ip;
-	break;
-case SORTF:
+	next();
+SORTF_label:
 	sort(posVectors[op->p1].begin(), posVectors[op->p1].end(),
 		[op](const valPos &a, const valPos &b){ return (a.val.f > b.val.f)^op->p2; });
 	++ip;
-	break;
-case SORTS:
+	next();
+SORTS_label:
 	sort(posVectors[op->p1].begin(), posVectors[op->p1].end(),
 		[op](const valPos &a, const valPos &b){ return (scomp(a.val.s, b.val.s) > 0)^op->p2; });
 	++ip;
-	break;
+	next();
 #endif
 
 //math operations
-case IADD:
+IADD_label:
 	if (ISNULL(stk0) || ISNULL(stk1)) stk1.b |= NIL;
 	else stk1.u.i += stk0.u.i;
 	pop();
 	++ip;
-	break;
-case FADD:
+	next();
+FADD_label:
 	if (ISNULL(stk0) || ISNULL(stk1)) stk1.b |= NIL;
 	else stk1.u.f += stk0.u.f;
 	pop();
 	++ip;
-	break;
-case TADD:
+	next();
+TADD_label:
 	strplus(stk1, stk0);
 	pop();
 	++ip;
-	break;
-case DRADD:
+	next();
+DRADD_label:
 	if (ISNULL(stk0) || ISNULL(stk1)) stk1.b |= NIL;
 	else { stk1.u.i += stk0.u.i; stk1.b = DR; }
 	pop();
 	++ip;
-	break;
-case DTADD:
+	next();
+DTADD_label:
 	if (ISNULL(stk0) || ISNULL(stk1)) stk1.b |= NIL;
 	else { stk1.u.i += stk0.u.i; stk1.b = DT; }
 	pop();
 	++ip;
-	break;
-case ISUB:
+	next();
+ISUB_label:
 	if (ISNULL(stk0) || ISNULL(stk1)) stk1.b |= NIL;
 	else stk1.u.i -= stk0.u.i;
 	pop();
 	++ip;
-	break;
-case FSUB:
+	next();
+FSUB_label:
 	if (ISNULL(stk0) || ISNULL(stk1)) stk1.b |= NIL;
 	else stk1.u.f -= stk0.u.f;
 	pop();
 	++ip;
-	break;
-case DTSUB:
+	next();
+DTSUB_label:
 	if (ISNULL(stk0) || ISNULL(stk1)) stk1.b |= NIL;
 	else { stk1.u.i -= stk0.u.i; stk1.b = DT; }
 	pop();
 	++ip;
-	break;
-case DRSUB:
+	next();
+DRSUB_label:
 	if (ISNULL(stk0) || ISNULL(stk1)) stk1.b |= NIL;
 	else { stk1.u.i -= stk0.u.i; stk1.b = DR; }
 	pop();
 	if (stk0.u.i < 0) stk0.u.i *= -1;
 	++ip;
-	break;
-case IMULT:
+	next();
+IMULT_label:
 	if (ISNULL(stk0) || ISNULL(stk1)) stk1.b |= NIL;
 	else stk1.u.i *= stk0.u.i;
 	pop();
 	++ip;
-	break;
-case FMULT:
+	next();
+FMULT_label:
 	if (ISNULL(stk0) || ISNULL(stk1)) stk1.b |= NIL;
 	else stk1.u.f *= stk0.u.f;
 	pop();
 	++ip;
-	break;
-case DRMULT:
+	next();
+DRMULT_label:
 	if (ISNULL(stk0) || ISNULL(stk1)) stk1.b |= NIL;
 	else {
 		iTemp1 = stk0.b; iTemp2 = stk1.b;
@@ -294,20 +299,20 @@ case DRMULT:
 	}
 	pop();
 	++ip;
-	break;
-case IDIV:
+	next();
+IDIV_label:
 	if (ISNULL(stk0) || ISNULL(stk1) || stk0.u.i==0) stk1.b |= NIL;
 	else stk1.u.i /= stk0.u.i;
 	pop();
 	++ip;
-	break;
-case FDIV:
+	next();
+FDIV_label:
 	if (ISNULL(stk0) || ISNULL(stk1) || stk0.u.f==0) stk1.b |= NIL;
 	else stk1.u.f /= stk0.u.f;
 	pop();
 	++ip;
-	break;
-case DRDIV:
+	next();
+DRDIV_label:
 	if (ISNULL(stk0) || ISNULL(stk1) || stk0.u.f==0) stk1.b |= NIL;
 	else {
 		if (stk0.b == I)
@@ -317,26 +322,26 @@ case DRDIV:
 	}
 	pop();
 	++ip;
-	break;
-case INEG:
+	next();
+INEG_label:
 	if (ISNULL(stk0)) stk0.b |= NIL;
 	else stk0.u.i *= -1;
 	++ip;
-	break;
-case FNEG:
+	next();
+FNEG_label:
 	if (ISNULL(stk0)) stk0.b |= NIL;
 	else stk0.u.f *= -1;
 	++ip;
-	break;
-case PNEG:
+	next();
+PNEG_label:
 	stk0.u.p ^= true;
 	++ip;
-	break;
+	next();
 
 //comparisions - p1 determines how far down the stack to leave the result
 // p2 is negator
 //may want to combine with jmp
-case IEQ:
+IEQ_label:
 	iTemp1 = ISNULL(stk0);
 	iTemp2 = ISNULL(stk1);
 	if (iTemp1 ^ iTemp2) stkp(op->p1).u.p = false;
@@ -344,8 +349,8 @@ case IEQ:
 	else stkp(op->p1).u.p = (stk1.u.i == stk0.u.i)^op->p2;
 	stacktop -= op->p1;
 	++ip;
-	break;
-case FEQ:
+	next();
+FEQ_label:
 	iTemp1 = ISNULL(stk0);
 	iTemp2 = ISNULL(stk1);
 	if (iTemp1 ^ iTemp2) stkp(op->p1).u.p = false;
@@ -353,8 +358,8 @@ case FEQ:
 	else stkp(op->p1).u.p = (stk1.u.f == stk0.u.f)^op->p2;
 	stacktop -= op->p1;
 	++ip;
-	break;
-case TEQ:
+	next();
+TEQ_label:
 	iTemp1 = ISNULL(stk0);
 	iTemp2 = ISNULL(stk1);
 	if (!(iTemp1|iTemp2)){ //none null
@@ -371,20 +376,20 @@ case TEQ:
 	}
 	stacktop -= op->p1;
 	++ip;
-	break;
-case ILEQ:
+	next();
+ILEQ_label:
 	if (ISNULL(stk0) || ISNULL(stk1)) stkp(op->p1).u.p = false;
 	else stkp(op->p1).u.p = (stk1.u.i <= stk0.u.i)^op->p2;
 	stacktop -= op->p1;
 	++ip;
-	break;
-case FLEQ:
+	next();
+FLEQ_label:
 	if (ISNULL(stk0) || ISNULL(stk1)) stkp(op->p1).u.p = false;
 	else stkp(op->p1).u.p = (stk1.u.f <= stk0.u.f)^op->p2;
 	stacktop -= op->p1;
 	++ip;
-	break;
-case TLEQ:
+	next();
+TLEQ_label:
 	if (ISNULL(stk0) || ISNULL(stk1)) stkp(op->p1).u.p = false;
 	else {
 		boolTemp = (scomp(stk1.u.s, stk0.u.s) <= 0)^op->p2;
@@ -394,20 +399,20 @@ case TLEQ:
 	}
 	stacktop -= op->p1;
 	++ip;
-	break;
-case ILT:
+	next();
+ILT_label:
 	if (ISNULL(stk0) || ISNULL(stk1)) stkp(op->p1).u.p = false;
 	else stkp(op->p1).u.p = (stk1.u.i < stk0.u.i)^op->p2;
 	stacktop -= op->p1;
 	++ip;
-	break;
-case FLT:
+	next();
+FLT_label:
 	if (ISNULL(stk0) || ISNULL(stk1)) stkp(op->p1).u.p = false;
 	else stkp(op->p1).u.p = (stk1.u.f < stk0.u.f)^op->p2;
 	stacktop -= op->p1;
 	++ip;
-	break;
-case TLT:
+	next();
+TLT_label:
 	if (ISNULL(stk0) || ISNULL(stk1)) stkp(op->p1).u.p = false;
 	else {
 		boolTemp = (scomp(stk1.u.s, stk0.u.s) < 0)^op->p2;
@@ -417,34 +422,34 @@ case TLT:
 	}
 	stacktop -= op->p1;
 	++ip;
-	break;
-case LIKE:
+	next();
+LIKE_label:
 	iTemp1 = !regexec(q->dataholder[op->p1].u.r, stk0.u.s, 0, 0, 0)^op->p2;
 	FREE2(stk0);
 	stk0.u.p = iTemp1;
 	++ip;
-	break;
+	next();
 
-case POP:
+POP_label:
 	FREE2(stk0);
 	pop();
 	++ip;
-	break;
-case POPCPY:
+	next();
+POPCPY_label:
 	FREE2(stk1);
 	stk1 = stk0;
 	DISOWN(stk0);
 	pop();
 	++ip;
-	break;
-case NULFALSE1:
+	next();
+NULFALSE1_label:
 	if (ISNULL(stk0)){
 		FREE2(stk0);
 		stk0.u.p = false;
 		ip = op->p1;
 	} else ++ip;
-	break;
-case NULFALSE2:
+	next();
+NULFALSE2_label:
 	if (ISNULL(stk0)){
 		FREE2(stk0);
 		pop();
@@ -452,10 +457,10 @@ case NULFALSE2:
 		stk0.u.p = false;
 		ip = op->p1;
 	} else ++ip;
-	break;
+	next();
 
 //type conversions
-case CVIS:
+CVIS_label:
 	if (!ISNULL(stk0)){
 		stk0.z = sprintf(bufTemp, "%lld", stk0.u.i);
 		stk0.u.s = (char*) malloc(stk0.z+1);
@@ -467,8 +472,8 @@ case CVIS:
 		}
 	}
 	++ip;
-	break;
-case CVFS:
+	next();
+CVFS_label:
 	if (!ISNULL(stk0)){
 		stk0.z = sprintf(bufTemp, "%.10g", stk0.u.f);
 		stk0.u.s = (char*) malloc(stk0.z+1);
@@ -480,22 +485,22 @@ case CVFS:
 		}
 	}
 	++ip;
-	break;
-case CVFI:
+	next();
+CVFI_label:
 	if (!ISNULL(stk0)){
 		stk0.u.i = stk0.u.f;
 		stk0.b = I;
 	}
 	++ip;
-	break;
-case CVIF:
+	next();
+CVIF_label:
 	if (!ISNULL(stk0)){
 		stk0.u.f = stk0.u.i;
 		stk0.b = F;
 	}
 	++ip;
-	break;
-case CVSI:
+	next();
+CVSI_label:
 	if (!ISNULL(stk0)){
 		iTemp1 = strtol(stk0.u.s, &cstrTemp, 10);
 		FREE2(stk0);
@@ -504,8 +509,8 @@ case CVSI:
 		if (*cstrTemp){ stk0.b |= NIL; }
 	}
 	++ip;
-	break;
-case CVSF:
+	next();
+CVSF_label:
 	if (!ISNULL(stk0)){
 		fTemp = strtof(stk0.u.s, &cstrTemp);
 		FREE2(stk0);
@@ -514,8 +519,8 @@ case CVSF:
 		if (*cstrTemp){ stk0.b |= NIL; }
 	}
 	++ip;
-	break;
-case CVSDT:
+	next();
+CVSDT_label:
 	if (!ISNULL(stk0)){
 		iTemp1 = dateparse(stk0.u.s, &i64Temp, &iTemp2, stk0.z);
 		FREE2(stk0);
@@ -525,8 +530,8 @@ case CVSDT:
 		if (iTemp1) stk0.b |= NIL;
 	}
 	++ip;
-	break;
-case CVSDR:
+	next();
+CVSDR_label:
 	if (!ISNULL(stk0)){
 		iTemp1 = parseDuration(stk0.u.s, &i64Temp);
 		FREE2(stk0);
@@ -535,8 +540,8 @@ case CVSDR:
 		if (iTemp1) stk0.b |= NIL;
 	}
 	++ip;
-	break;
-case CVDRS:
+	next();
+CVDRS_label:
 	if (!ISNULL(stk0)){
 		stk0.u.s = (char*) malloc(24);
 		durstring(stk0.u.i, stk0.u.s);
@@ -544,8 +549,8 @@ case CVDRS:
 		stk0.z = strlen(stk0.u.s);
 	}
 	++ip;
-	break;
-case CVDTS:
+	next();
+CVDTS_label:
 	if (!ISNULL(stk0)){
 		//make version of datestring that writes directly to arg buf
 		cstrTemp = datestring(stk0.u.i);
@@ -555,30 +560,30 @@ case CVDTS:
 		stk0.z = 19;
 	}
 	++ip;
-	break;
+	next();
 
 //jump instructions
-case JMP:
+JMP_label:
 	ip = op->p1;
-	break;
-case JMPCNT:
+	next();
+JMPCNT_label:
 	ip = (numPrinted < quantityLimit) ? op->p1 : ip+1;
-	break;
-case JMPFALSE:
+	next();
+JMPFALSE_label:
 	ip = !stk0.u.p ? op->p1 : ip+1;
 	if (op->p2 == 1){
 		FREE2(stk0);
 		pop();
 	}
-	break;
-case JMPTRUE:
+	next();
+JMPTRUE_label:
 	ip = stk0.u.p ? op->p1 : ip+1;
 	if (op->p2 == 1){
 		FREE2(stk0);
 		pop();
 	}
-	break;
-case JMPNOTNULL_ELSEPOP:
+	next();
+JMPNOTNULL_ELSEPOP_label:
 	if (ISNULL(stk0)){
 		FREE2(stk0);
 		pop();
@@ -586,9 +591,9 @@ case JMPNOTNULL_ELSEPOP:
 	} else {
 		ip = op->p1;
 	}
-	break;
+	next();
 
-case PRINT:
+PRINT_label:
 	for (int i=0; i<torowSize; ++i){
 		torow[i].print();
 		if (i < torowSize-1) fmt::print(",");
@@ -596,10 +601,10 @@ case PRINT:
 	fmt::print("\n");
 	++numPrinted;
 	++ip;
-	break;
+	next();
 
 //distinct checkers
-case NDIST:
+NDIST_label:
 	boolTemp = bt_nums[op->p2].insert(stk0.u.i).second;
 	if (boolTemp) {
 		distinctVal = stk0;
@@ -608,8 +613,8 @@ case NDIST:
 		ip = op->p1;
 	}
 	pop();
-	break;
-case SDIST:
+	next();
+SDIST_label:
 	boolTemp = bt_strings[op->p2].insert(treeCString(stk0)).second;
 	if (boolTemp) {
 		if (op->p3){ //not hidden
@@ -625,41 +630,46 @@ case SDIST:
 		ip = op->p1;
 	}
 	pop();
-	break;
+	next();
 
 //functions
-case FINC:
+FINC_label:
 	q->dataholder[op->p1].u.f++;
 	push();
 	stk0 = q->dataholder[op->p1];
 	++ip;
-	break;
-case ENCCHA:
+	next();
+ENCCHA_label:
 	pairTemp = q->crypt.chachaEncrypt(op->p1, stk0.z, stk0.u.s);
 	if (stk0.b & MAL) free(stk0.u.s);
 	stk0.u.s = pairTemp.first;
 	stk0.z = pairTemp.second;
 	stk0.b = T|MAL;
 	++ip;
-	break;
-case DECCHA:
+	next();
+DECCHA_label:
 	pairTemp = q->crypt.chachaDecrypt(op->p1, stk0.z, stk0.u.s);
 	if (stk0.b & MAL) free(stk0.u.s);
 	stk0.u.s = pairTemp.first;
 	stk0.z = pairTemp.second;
 	stk0.b = T|MAL;
 	++ip;
-	break;
+	next();
 
-case ENDRUN:
+ENDRUN_label:
 	goto endloop;
-case 0:
-	error("Invalid opcode");
-default:
-	error("Unknown opcode");
 
-		} //end big switch
-	} //end loop
+//unimplemeted opcodes
+CVER_label:
+CVNO_label:
+FEXP_label:
+IEXP_label:
+IMOD_label:
+CVDRF_label:
+DNEG_label:
+NEQ_label:
+	error("Invalid opcode");
+
 
 	endloop:
 	return;
