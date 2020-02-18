c061b00563f0fa5cecab71c1c29fd42b6343b573
dvhar <dh22@protonmail.com>
2020-02-17 19:09:07 -0600

    vars work in distinct filter

--- build/integrationTest.py
+++ build/integrationTest.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 from subprocess import Popen
 
+picky = False
+picks= [3,4] #which tests to run when picky
+
 f1 = "'test/country.csv' nh"
 f2 = "'test/cities.csv'"
 
 # [description, expected return code, query]
 tests = [
 ["Basic top 10",0,f"select top 10 from {f1}"],
-["With int and arithmetic",0,
-	"with c7 as int select top 10 int, c9, int+c9, int+c1, int+c7 "
-	"c1+c2 as ss, c1+c9 as sf, c1+c7 as si, c7+c9 as if, "
-	"int+-c5 as ii, int-c5 as ii2 c16, c16+'10 years', c16-'10 years', '10 years'*-2 "
-	f"from {f1}"]
+["With int and arithmetic",0, "with c7 as int select top 10 int, c9, int+c9, int+c1, int+c7 " "c1+c2 as ss, c1+c9 as sf, c1+c7 as si, c7+c9 as if, "
+	f"int+-c5 as ii, int-c5 as ii2 c16, c16+'10 years', c16-'10 years', '10 years'*-2 from {f1}"],
+["distinct string, case",0,f"select distinct c3 c15 case when c15 like '_F' or c15 like '%O' then 1 when c15 like '_i' then 2 else 0 end from {f1}"],
+["distinct var",0,f"with c5 as int select distinct int, int from {f1}"],
+["distinct hidden",0,f"with c5 as int select distinct hidden int, int from {f1}"],
 ]
 
 def runtest(test):
@@ -29,7 +32,11 @@ def runtest(test):
 	print(f"Success! return code: {ret.returncode}")
 
 
-for test in tests:
-	runtest(test)
+for i,test in enumerate(tests):
+	if picky:
+		if i in picks:
+			runtest(test)
+	else:
+		runtest(test)
 
 print("All tests passed!")
