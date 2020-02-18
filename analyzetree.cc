2a30c16ea59de51d15fcac7af8e5d3e420a9aa57
dvhar <=>
2020-02-17 19:09:07 -0600

    vars work in distinct filter

--- analyzetree.cc
+++ analyzetree.cc
@@ -1,38 +1,45 @@
 #include "interpretor.h"
 
-
-static void varUsedInWhere(unique_ptr<node> &n, querySpecs &q){
+static void varUsedInFilter(unique_ptr<node> &n, querySpecs &q){
 	if (n == nullptr) return;
 	static int filterBranch = 0;
+	string t1;
 	switch (n->label){
 	//skip irrelvent subtrees
 	case N_PRESELECT:
-	case N_SELECT:
 	case N_FROM:
 	case N_GROUPBY:
 	case N_ORDER:
 	case N_HAVING: //may use this one later
 		break;
+	case N_SELECTIONS:
+		t1 = n->tok1.lower();
+		if (t1 == "hidden" || t1 == "distinct"){
+			filterBranch = 1;
+			varUsedInFilter(n->node1, q);
+			filterBranch = 0;
+		}
+		varUsedInFilter(n->node2, q);
+		break;
 	case N_WHERE:
 		filterBranch = 1;
-		varUsedInWhere(n->node1, q);
+		varUsedInFilter(n->node1, q);
 		filterBranch = 0;
 		break;
 	case N_VALUE:
 		if (filterBranch && n->tok2.id == VARIABLE){
-			cerr << "checking " << n->tok1.val << endl;
 			for (auto &v : q.vars)
 				if (n->tok1.val == v.name){
-					v.filter = filterBranch;
+					v.filter |= filterBranch;
 					cerr << n->tok1.val << " used in filter\n";
 				}
 		}
 		break;
 	default:
-		varUsedInWhere(n->node1, q);
-		varUsedInWhere(n->node2, q);
-		varUsedInWhere(n->node3, q);
-		varUsedInWhere(n->node4, q);
+		varUsedInFilter(n->node1, q);
+		varUsedInFilter(n->node2, q);
+		varUsedInFilter(n->node3, q);
+		varUsedInFilter(n->node4, q);
 	}
 }
 
@@ -51,9 +58,8 @@ static void recordResultColumns(unique_ptr<node> &n, querySpecs &q){
 	string t1 = n->tok1.lower();
 	switch (n->label){
 	case N_SELECTIONS:
-		if (t1 == "hidden")
-			return;
-		else if (t1 == "*"){
+		if (t1 == "hidden") {
+		} else if (t1 == "*"){
 			selectAll(q);
 		} else {
 			q.colspec.count++;
@@ -67,6 +73,7 @@ static void recordResultColumns(unique_ptr<node> &n, querySpecs &q){
 			selectAll(q);
 		break;
 	default:
+		def:
 		recordResultColumns(n->node1, q);
 		recordResultColumns(n->node2, q);
 		recordResultColumns(n->node3, q);
@@ -76,6 +83,6 @@ static void recordResultColumns(unique_ptr<node> &n, querySpecs &q){
 
 //typing done, still need semantics etc
 void analyzeTree(querySpecs &q){
-	varUsedInWhere(q.tree, q);
+	varUsedInFilter(q.tree, q);
 	recordResultColumns(q.tree, q);
 }
