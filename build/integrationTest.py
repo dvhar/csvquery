df3bf57796fc8ac6dfe6778570d0d6c6797c172e
dvhar <dh22@protonmail.com>
2020-05-08 21:51:05 -0500

    fix test file encoding

--- build/integrationTest.py
+++ build/integrationTest.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
-#TODO: check actual results instead of just return code after csv formatting is finalized
-
-from subprocess import Popen
+import subprocess
 import json
 
 picky = False
@@ -12,27 +10,29 @@ f1 = "'test/country.csv' nh"
 f2 = "'test/cities.csv'"
 
 def runtest(test):
-	q = test['query'] % {'f1':f1, 'f2':f2}
-	print("===============================================================")
-	print(test['title'])
-	print("---------------------------------------")
-	print(q)
-	print("---------------------------------------")
-	ret = Popen(["./cql","-c", q])
-	ret.communicate()
-	if ret.returncode != test['code']:
-		print(f"Failed! Expected return code {test[1]} but got {ret.returncode}")
-		quit()
-	print("---------------------------------------")
-	print(f"Success! return code: {ret.returncode}")
+    q = test['query'] % {'f1':f1, 'f2':f2}
+    print("===============================================================")
+    print(test['title'])
+    print("---------------------------------------")
+    print(q)
+    print("---------------------------------------")
+    ret = subprocess.Popen(["./cql","-c", q], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    out, err = ret.communicate()
+    out = "".join(map(chr,out))
+    print(out)
+    if ret.returncode != test['code']:
+        print(f"Failed! Expected return code {test[1]} but got {ret.returncode}")
+        quit()
+    print("---------------------------------------")
+    print(f"Success! return code: {ret.returncode}")
 
 with open('testdata.json') as testfile:
-	tests = json.load(testfile)
-	for test in tests:
-		if picky:
-			if i in picks:
-				runtest(test)
-		else:
-			runtest(test)
+    tests = json.load(testfile)
+    for test in tests:
+        if picky:
+            if i in picks:
+                runtest(test)
+        else:
+            runtest(test)
 
 print("All tests passed!")
