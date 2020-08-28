245927039fda91c760fb786af85816a8335d0f1b
dvhar <dh22@protonmail.com>
2020-08-28 17:37:04 -0500

    linereader prototype

--- deps/experimental/bufreader.cc
+++ deps/experimental/bufreader.cc
@@ -1,66 +1,47 @@
 #include<string.h>
 #include<iostream>
-#include<fstream>
+#include "bufreader.h"
 using namespace std;
 
-#define CUSTOM
+inline void lreader::refresh(){
+	auto readb = fread(buf, 1, BS, f);
+	line = buf;
+	end = line + readb;
+	nl = (char*) memchr(line, '\n', end - line);
+	if (nl) *nl = 0;
+	else line = NULL;
+}
+inline void lreader::realign(){
+	memmove(buf, line, end-line);
+}
 
-const int BS = 1024 * 1024 * 8;
-char buf[BS];
-char* fname =  (char*) "../../build/ptest.csv";
-#ifdef CUSTOM
-auto f = fopen(fname,"r");
-char* readline(){
-	static char* end;
-	static char* nl;
-	static char* line = NULL;
+char* lreader::readline(){
 	if (line == NULL){
-		auto readb = fread(buf, 1, BS, f);
-		line = buf;
-		end = line + readb;
-		nl = (char*) memchr(line, '\n', end - line);
-		*nl = 0;
+		refresh();
 	} else {
-		if (end - line > 0){
+		if (end > line){
 			line = nl + 1;
 			nl = (char*) memchr(line, '\n', end - line);
 			if (nl){
 				*nl = 0;
 			} else {
-				int rem = end - line;
-				memcpy(buf, line, rem);
-				auto readb = fread(buf+rem, 1, BS-rem, f);
-				line = buf;
-				end = line + BS;
-				nl = (char*) memchr(line, '\n', end - line);
-				*nl = 0;
+				realign();
+				refresh();
 			}
 		} else {
-				int rem = end - line;
-				memcpy(buf, line, rem);
-				auto readb = fread(buf+rem, 1, BS-rem, f);
-				line = buf;
-				end = line + BS;
-				nl = (char*) memchr(line, '\n', end - line);
-				*nl = 0;
+				realign();
+				refresh();
 		}
 	}
 	return line;
 }
-#else
-ifstream ff(fname);
-#endif
 
 int main(int argc, char** argv){
 	int count = 0;
-#ifdef CUSTOM
-	while (!feof(f)){
-		readline();
+	char* line;
+	auto r = lreader((char*) "/home/dave/gits/csvquery/build/test/country.csv");
+	while ((line = r.readline())){
+		cout << "line: '" << line << "'" << endl;
 	}
-#else
-	while (ff.good()){
-		ff.getline(buf, BS);
-	}
-#endif
 	return 0;
 }
