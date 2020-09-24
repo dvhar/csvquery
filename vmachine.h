e4d3fca59ce34770a0cdd6ae18cf4636f700e7ca
dvhar <dh22@protonmail.com>
2020-09-23 19:52:51 -0500

    start messager

--- vmachine.h
+++ vmachine.h
@@ -39,7 +39,8 @@ enum codes : int {
 	JOINSET_INIT, JOINSET_TRAV, AND_SET, OR_SET,
 	SAVEANDCHAIN, SORT_ANDCHAIN,
 	FUNCYEAR, FUNCMONTH, FUNCWEEK, FUNCYDAY, FUNCMDAY, FUNCWDAY, FUNCHOUR, FUNCMINUTE, FUNCSECOND, FUNCWDAYNAME, FUNCMONTHNAME,
-	FUNCABSF, FUNCABSI
+	FUNCABSF, FUNCABSI,
+	START_MESSAGE, STOP_MESSAGE
 };
 
 //2d array for ops indexed by operation and datatype
@@ -149,6 +150,37 @@ class stddev {
 	}
 };
 
+class messager {
+	unique_ptr<thread> runner;
+	forward_list<bool> running;
+	int blank = 0;
+	public:
+	static const int scanning = 0;
+	static const int retrieving = 1;
+	static const int sorting = 2;
+	void say(char* msg, int* n1, int* n2){
+		fprintf(stderr, msg, *(n1?:&blank), *(n2?:&blank));
+	}
+	void start(char* msg, int* n1, int* n2){
+		running.push_front(true);
+		runner.reset(new thread([&](char* msg,int* num1,int* num2){
+			bool* active = &running.front();
+			while(1){
+				sleep(1);
+				if (!*active)
+					return;
+				fprintf(stderr, msg, *num1, *num2);
+			}
+		}, msg, n1?:&blank, n2?:&blank));
+		runner->detach();
+	}
+	void stop(){
+		running.front() = false;
+		runner.reset(nullptr);
+		cerr << "r\33[2K";
+	}
+};
+
 class rowgroup;
 class singleQueryResult;
 class vmachine {
@@ -175,6 +207,7 @@ class vmachine {
 	string outbuf;
 	ostream csvOutput;
 	ofstream outfile;
+	messager updates;
 	//datunion comparers
 	static const function<bool (const datunion, const datunion&)> uLessFuncs[3];
 	static const function<bool (const datunion, const datunion&)> uGrtFuncs[3];
