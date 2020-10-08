acad4f9bff1e22f7decdc4f1b491a1d1610dedc2
dvhar <dh22@protonmail.com>
2020-10-07 22:26:31 -0500

    add remote host blocking to embedded site

--- embed_site.cc
+++ embed_site.cc
@@ -3,6 +3,14 @@
 #include <string_view>
 using HttpServer = SimpleWeb::Server<SimpleWeb::HTTP>;
 using namespace std;
+static auto localhost = boost::asio::ip::address::from_string("::1");
+static bool rejectNonLocals(shared_ptr<HttpServer::Request>& request){
+	if (request->remote_endpoint().address() != localhost){
+		cerr << "attempted connection from non-localhost\n";
+		return true;
+	}
+	return false;
+}
 void embedsite(HttpServer &server){
 #include "embed_site.hpp"
 }
