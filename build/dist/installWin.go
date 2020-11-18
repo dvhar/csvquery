9c2ff9251544e2eb428fe0cc63641b92bfba775a
dvhar <d@d.d>
2020-11-17 20:25:21 -0800

    update installer

--- build/dist/installWin.go
+++ build/dist/installWin.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"time"
 	"io"
 	"io/ioutil"
 	"log"
@@ -17,20 +18,51 @@ import (
 
 var baseurl string = `https://davosaur.com/csv/`
 var zipurlfmt string = baseurl + `w/csvquery-win-%s.zip`
-var versionurl string = baseurl + `?d=version`
+var infoUrl string = baseurl + `?d=install`
 var installPath string = `C:\Program Files\Csvquery`
 var programPath string = installPath + `\csvquery\csvquery.exe`
-var testing bool = true
+var desktopPath string = os.Getenv(`USERPROFILE`)+`\Desktop\csvquery`
+var versionInfo info
 
 type info struct {
-	Version string `json:"version"`
+	Version      string `json:"version"`
+	InstallNotes string `json:"notes"`
 }
 
 func main() {
 	fmt.Println(title)
-	ok, version := shouldInstall()
-	if ok {
-		install(version, installPath)
+	getVersionInfo()
+	if needInstall() {
+		fmt.Println(`Press Enter to install or update csvquery`)
+		prompt()
+		install()
+	}
+	makeShortcut()
+	fmt.Printf("\n%s\n",versionInfo.InstallNotes)
+	fmt.Println(`Installation/update Complete. You may close this window.`)
+	time.Sleep(time.Minute)
+}
+
+func prompt() bool {
+	var input string
+	fmt.Scanln(&input)
+	return strings.TrimSpace(strings.ToLower(input)) == `y`
+}
+
+func makeShortcut(){
+	if _,err := os.Stat(desktopPath); err == nil {
+		fmt.Println("Desktop shortcut exists")
+		return
+	}
+	fmt.Printf("\nCreate desktop shortcut? Enter 'y' if yes\n")
+	if prompt() == false {
+		return
+	}
+	cmd := exec.Command(`cmd`, `/c`, `mklink`, desktopPath, programPath)
+	out, err := cmd.CombinedOutput()
+	fmt.Println(string(out))
+	if err != nil {
+		fmt.Println(err)
 	}
 }
 
@@ -48,43 +80,41 @@ func downloadBytes(url string) ([]byte, error) {
 	return ioutil.ReadAll(resp.Body)
 }
 
-func newestVersion() string {
-	infobytes, err := downloadBytes(versionurl)
+func getVersionInfo() {
+	infobytes, err := downloadBytes(infoUrl)
 	if err != nil {
 		log.Fatalf("Failed to find out the newest version: %v", err)
 	}
-	var v info
-	chk(json.Unmarshal(infobytes, &v))
-	return strings.TrimSpace(v.Version)
+	chk(json.Unmarshal(infobytes, &versionInfo))
+	versionInfo.Version = strings.TrimSpace(versionInfo.Version)
 }
 
-func shouldInstall() (bool, string) {
+func needInstall() bool {
 	//check local version
 	cmd := exec.Command(programPath, `version`)
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		return true, newestVersion()
+		return true
 	}
 	current := strings.TrimSpace(string(out))
 	fmt.Println("Current version:", current)
 
 	//check newest version
-	newversion := newestVersion()
-	fmt.Println("Newest version:", newversion)
-	if newversion == current {
+	fmt.Println("Newest version:", versionInfo.Version)
+	if versionInfo.Version == current {
 		fmt.Println("Your version of csvquery is up to date.")
-		return false, ""
+		return false
 	}
-	return true, newversion
+	return true
 }
 
-func install(version, destdir string) {
-	zipped, err := downloadBytes(fmt.Sprintf(zipurlfmt, version))
+func install() {
+	fmt.Println(installing)
+	zipped, err := downloadBytes(fmt.Sprintf(zipurlfmt, versionInfo.Version))
 	if err != nil {
 		log.Fatalf("Failed to download program: %v", err)
 	}
-	fmt.Println(installing)
-	created, err := extract(zipped, destdir)
+	created, err := extract(zipped)
 	chk(err)
 	fmt.Println("Created files:")
 	for _, f := range created {
@@ -92,27 +122,33 @@ func install(version, destdir string) {
 	}
 }
 
+func uninstall() {
+	os.RemoveAll(installPath)
+	os.RemoveAll(desktopPath)
+	fmt.Println(`Uninstalled`)
+	time.Sleep(time.Minute)
+	os.Exit(0)
+}
+
 func chk(err error) {
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
+		time.Sleep(time.Minute)
 	}
 }
 
-func extract(zipped []byte, destdir string) ([]string, error) {
+func extract(zipped []byte) ([]string, error) {
 	var filenames []string
 	brd := bytes.NewReader(zipped)
 	r, err := zip.NewReader(brd, int64(len(zipped)))
 	chk(err)
 	for _, f := range r.File {
-		fpath := filepath.Join(destdir, f.Name)
+		fpath := filepath.Join(installPath, f.Name)
 		// Check for ZipSlip. More Info: http://bit.ly/2MsjAWE
-		if !strings.HasPrefix(fpath, filepath.Clean(destdir)+string(os.PathSeparator)) {
+		if !strings.HasPrefix(fpath, filepath.Clean(installPath)+string(os.PathSeparator)) {
 			return filenames, fmt.Errorf("%s: illegal file path", fpath)
 		}
 		filenames = append(filenames, fpath)
-		if testing {
-			continue
-		}
 		if f.FileInfo().IsDir() {
 			// Make Folder
 			os.MkdirAll(fpath, os.ModePerm)
