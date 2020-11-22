7b7ffce09de6e1a8c4736bdb36fedf8c5b0fea7f
dvhar <dh22@protonmail.com>
2020-11-22 11:56:15 -0600

    1.20

--- utils.cc
+++ utils.cc
@@ -5,7 +5,7 @@
 #include "deps/json/escape.h"
 #define max(a,b) (a) > (b) ? (a) : (b)
 
-string version = "1.19";
+string version = "1.20";
 int runmode;
 regex_t leadingZeroString;
 regex_t durationPattern;
