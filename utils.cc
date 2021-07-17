694c7d34aae9752606fdd96883aa2e87f4badb02
dvhar <dh22@protonmail.com>
2021-07-17 14:42:13 -0500

    1.30

--- utils.cc
+++ utils.cc
@@ -6,7 +6,7 @@
 #include "deps/json/escape.h"
 #define max(a,b) (a) > (b) ? (a) : (b)
 
-string version = "1.29";
+string version = "1.30";
 int runmode;
 regex_t leadingZeroString;
 regex_t durationPattern;
