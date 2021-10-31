699f05df316efa91a480a3ceade20c7b17888876
dvhar <dh22@protonmail.com>
2021-10-31 15:35:49 -0500

    update tests with standards compliant distinct

--- utils.cc
+++ utils.cc
@@ -6,7 +6,7 @@
 #include "deps/json/escape.h"
 #define max(a,b) (a) > (b) ? (a) : (b)
 
-string version = "1.30";
+string version = "1.40";
 int runmode;
 regex_t leadingZeroString;
 regex_t durationPattern;
