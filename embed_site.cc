f39ffed8006ba1e4f30247d6d028965cf5d09d02
dvhar <dh22@protonmail.com>
2022-10-29 21:01:32 -0500

    html returned but not processed yet

--- embed_site.cc
+++ embed_site.cc
@@ -14,4 +14,14 @@ static bool rejectNonLocals(shared_ptr<HttpServer::Request>& request){
 	return false;
 }
 
-#include "embed_site.hpp"
+
+INCBIN(1,"../newgui/index.html");
+INCBIN(2,"../newgui/main.js");
+INCBIN(3,"../newgui/style.css");
+
+void embedsite(HttpServer &server){
+server.default_resource["GET"] = [](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request){ if (rejectNonLocals(request)) return; response->write(string_view((const char*)g1Data, g1Size)); };
+server.resource["^/main.js$"]["GET"] = [](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request){ if (rejectNonLocals(request)) return; response->write(string_view((const char*)g2Data, g2Size)); };
+server.resource["^/style.css$"]["GET"] = [](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request){ if (rejectNonLocals(request)) return; response->write(string_view((const char*)g3Data, g3Size)); };
+}
+
